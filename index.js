51cd8837e9699ae52c5c726ecd3c9c5a27e1febd
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-05 21:24:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,3 @@ function happyHolidaysTo(greeting, name) {
   console.log('Happy holidays, ${name}')
 }
 happyHolidaysTo(you)
-
-function sayHelloTo(firstName) {
-  console.log(`Hello, ${firstName}!`)
-}
- 
-sayHelloTo('Isabel') // "Hello, Isabel!"
-sayHelloTo("Jane") // "Hello, Jane!"
-sayHelloTo("R2-D2") // "Hello, R2-D2!"
-sayHelloTo(1) // "Hello, 1!"
- 
